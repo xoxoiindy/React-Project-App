678253093ee2bafdd4d3bad180128dc168a6fe10
Cindy <proanocindy@gmail.com>
2022-07-21 17:41:15 -0600

    campsitesSlice

--- src/features/campsites/CampsiteCard.js
+++ src/features/campsites/CampsiteCard.js
@@ -2,24 +2,17 @@ import React from 'react';
 import {Card, CardImg, CardImgOverlay, CardTitle} from 'reactstrap';
 
 
-const CampsiteCard = props => {
-
-
+const CampsiteCard = ({ campsite }) => {
+    const { image, name } = campsite;
     return (
         <Card>
-        <CardImg 
-        width='100%'
-        src={props.campsite.image}
-        alt={props.campsite.name}
-        />
-
-        <CardImgOverlay>
-            <CardTitle>{props.campsite.name}</CardTitle>
-        </CardImgOverlay>
+            <CardImg width='100%' src={image} alt={name} />
+            <CardImgOverlay>
+                <CardTitle>{name}</CardTitle>
+            </CardImgOverlay>
         </Card>
-        );
-  
-    }
+    );
+};
 
   export default CampsiteCard;
 
