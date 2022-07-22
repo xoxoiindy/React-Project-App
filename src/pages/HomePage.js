f9f8dd5986c8245fe854634195d6daee2af40600
Cindy <proanocindy@gmail.com>
2022-07-22 17:29:25 -0600

    Breadcrumbs

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,14 @@
 import { Container } from 'reactstrap';
 import DisplayList from '../features/display/DisplayList';
+import SubHeader from '../components/SubHeader';
 
 const HomePage = () => {
     return (
         <Container>
+             <SubHeader current='Home' detail={true} />
+            
             <DisplayList />
+            
         </Container>
     );
 };
