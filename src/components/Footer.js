456d0e2010c7819c3bbe91634454f044a496d35b
Cindy <proanocindy@gmail.com>
2022-07-22 16:47:45 -0600

    HomePage Display

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,41 +1,56 @@
-
+import { Container, Row, Col } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
         <footer className='site-footer'>
-            <div className='container'>
-                <div className='row'>
-                    <div className='col' xs={{ size: 4, offset: 1 }} sm='2'>
-                        <h5>this will be site navigation!</h5>
-                    </div>
-                    <div className='col text-center' xs='6' sm='3'>
+            <Container>
+                <Row>
+                    <Col xs={{ size: 4, offset: 1 }} sm='2'>
+                        <h5>Links</h5>
+                        <ul className='list-unstyled'>
+                            <li>
+                                <Link to='/'>Home</Link>
+                            </li>
+                            <li>
+                                <Link to='/directory'>Directory</Link>
+                            </li>
+                            <li>
+                                <Link to='/about'>About</Link>
+                            </li>
+                            <li>
+                                <Link to='/contact'>Contact</Link>
+                            </li>
+                        </ul>
+                    </Col>
+                    <Col xs='6' sm='3' className='text-center'>
                         <h5>Social</h5>
-                            <a
-                                className='btn btn-social-icon btn-instagram'
-                                href='http://instagram.com/'
-                            >
-                                <i className='fa fa-instagram' />
-                            </a>{' '}
-                            <a
-                                className='btn btn-social-icon btn-facebook'
-                                href='http://www.facebook.com/'
-                            >
-                                <i className='fa fa-facebook' />
-                            </a>{' '}
-                            <a
-                                className='btn btn-social-icon btn-twitter'
-                                href='http://twitter.com/'
-                            >
-                                <i className='fa fa-twitter' />
-                            </a>{' '}
-                            <a
-                                className='btn btn-social-icon btn-google'
-                                href='http://youtube.com/'
-                            >
-                                <i className='fa fa-youtube' />
-                            </a>
-                    </div>
-                    <div className='col text-center'  sm='4'>
+                        <a
+                            className='btn btn-social-icon btn-instagram'
+                            href='http://instagram.com/'
+                        >
+                            <i className='fa fa-instagram' />
+                        </a>{' '}
+                        <a
+                            className='btn btn-social-icon btn-facebook'
+                            href='http://www.facebook.com/'
+                        >
+                            <i className='fa fa-facebook' />
+                        </a>{' '}
+                        <a
+                            className='btn btn-social-icon btn-twitter'
+                            href='http://twitter.com/'
+                        >
+                            <i className='fa fa-twitter' />
+                        </a>{' '}
+                        <a
+                            className='btn btn-social-icon btn-google'
+                            href='http://youtube.com/'
+                        >
+                            <i className='fa fa-youtube' />
+                        </a>
+                    </Col>
+                    <Col sm='4' className='text-center'>
                         <a
                             role='button'
                             className='btn btn-link'
@@ -51,16 +66,11 @@ const Footer = () => {
                         >
                             <i className='fa fa-envelope-o' /> campsites@nucamp.co
                         </a>
-                        
-                    </div>
-
-                </div>
-
-            </div>
-
+                    </Col>
+                </Row>
+            </Container>
         </footer>
-    )
-}
+    );
+};
 
 export default Footer;
-
