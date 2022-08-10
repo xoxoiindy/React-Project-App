96e2fac6ded9aa938913b98558bf977628bbea3e
Cindy <proanocindy@gmail.com>
2022-08-09 23:08:14 -0600

    Error and Loading

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import { Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Routes, Route} from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import ContactPage from './pages/ContactPage';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
@@ -6,9 +8,17 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import CampsiteDetailPage from './pages/CampsiteDetailPage';
+import { fetchCampsites } from './features/campsites/campsitesSlice';
 import './App.css';
 
 function App() {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchCampsites());
+    }, [dispatch]);
+
+
     return (
         <div className='App'>
             <Header />
